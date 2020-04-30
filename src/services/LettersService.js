802d20eb426daeeb6b49b5397c7f9226381dbd09
jay-banx <golovchenko.dennis@gmail.com>
2020-05-01 01:16:44 +0300

    Updated LettersService

--- src/services/LettersService.js
+++ src/services/LettersService.js
@@ -1,93 +1,30 @@
 class LettersService {
-  _posts = [
-    {
-      id: 0,
-      user: {
-        id: 0,
-        username: "Denis",
-        image: "https://picsum.photos/30",
-      },
-      date: 1587990994977,
-      content: "hello",
-      location: null,
-      likes: 0,
-      comments: [
-        {
-          id: 0,
-          user: {
-            id: 2,
-            username: "Robert",
-            image: "https://picsum.photos/30",
-          },
-          date: 1587990994977,
-          content: "uuuu",
-          likes: 0,
-        },
-        {
-          id: 1,
-          user: {
-            id: 3,
-            username: "Hugo",
-            image: "https://picsum.photos/30",
-          },
-          date: 1587990994977,
-          content: "ffff",
-          likes: 0,
-        },
-      ],
-    },
-    {
-      id: 1,
-      user: {
-        id: 1,
-        username: "Anna",
-        image: "https://picsum.photos/30",
-      },
-      date: 1587990094977,
-      content: "world",
-      location: null,
-      likes: 5,
-      comments: [],
-    },
-  ];
+  _apiBase = "http://localhost:3001";
 
-  _users = [
-    {
-      id: 0,
-      username: "Denis",
-      image: "https://picsum.photos/30",
-    },
-    {
-      id: 1,
-      username: "Anna",
-      image: "https://picsum.photos/30",
-    },
-    {
-      id: 2,
-      username: "Robert",
-      image: "https://picsum.photos/30",
-    },
-    {
-      id: 3,
-      username: "Hugo",
-      image: "https://picsum.photos/30",
-    },
-  ];
+  getResource = async (url) => {
+    const res = fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, recieved ${res.status}`);
+    }
+
+    return await res.json();
+  };
 
   getAllPosts = async () => {
-    return this._posts;
+    return this.getResource("/posts");
   };
 
-  getPost = async (id) => {
-    return this._posts.find((item) => item.id === id);
+  getPostComments = async (postId) => {
+    return this.getResource(`/comments?postId=${postId}`);
   };
 
-  getAllUsers = async () => {
-    return this._users;
+  getPostLikes = async (postId) => {
+    return this.getResource(`/likes?postId=${postId}`);
   };
 
-  getUser = async (id) => {
-    return this._users.find((item) => item.id === id);
+  getUser = async (userId) => {
+    return this.getResource(`/users/${userId}`);
   };
 }
 
